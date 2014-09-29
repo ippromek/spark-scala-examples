21e48c8f278530d0aff5df9493b7920dbdf088bb
alexander-bogdanov <alexander.bogdanov.ru@gmail.com>
2014-09-29 21:58:53 +0400

    Initial commit

---
+++ src/scala/org/abogdanov/WordCount.scala
@@ -0,0 +1,29 @@
+package org.abogdanov
+
+import org.apache.spark.SparkContext._
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+	def main(args: Array[String]) {
+
+		if (args.length < 2) {
+			System.err.println("Usage: \n" +
+				"./bin/spark-submit --class org.abogdanov.WordCount ./<jarName>")
+			System.exit(1)
+		}
+
+		// Create a Scala Spark Context
+		val conf = new SparkConf().setMaster("local").setAppName("WordCountApp")
+		val sc = new SparkContext(conf)
+
+		// Load our input data
+		val input = sc.textFile(args(0))
+		// Split it up into words.
+		val words = input.flatMap(line => line.split(" "))
+		// Transform into word and count.
+		val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
+
+		// Save the word count back out to a text file, causing evaluation.
+		counts.saveAsTextFile(args(1))
+	}
+}
